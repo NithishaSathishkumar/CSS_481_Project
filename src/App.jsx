994874cd6c72953c55340efb504050a7cc9f678f
NithishaSathishkumar <nithisha132004@gmail.com>
2024-11-28 17:42:07 -0800

    updated the firebase

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ function App() {
             <FilterTutor />
           </Layout>
         } />
+
       </Routes>
     </Router>
   );
--- src/Firebase/FirebaseConfig.jsx
+++ src/Firebase/FirebaseConfig.jsx
@@ -21,5 +21,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const fire = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export default fire;
