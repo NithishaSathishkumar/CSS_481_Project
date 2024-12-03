9e496b56f2c9e02f8e770f84c4a10b4e32c7ff51
sahong <shsong@uw.edu>
2024-12-03 01:05:49 -0800

    have done for the the tutor's img,username, primarysubject,availableTime, and price with the realtime database on the booking page, and without change anything on other pages

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../Styling/BookingPage.css';
 import tutor from '../assets/16.png';
 import clock from '../assets/17.png';
@@ -8,23 +8,43 @@ import chat from '../assets/chat.png';
 import globe from '../assets/globe.png';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { Link } from 'react-router-dom';  // import Link
+import { Link, useParams } from 'react-router-dom';  // import Link
+import { getDatabase, ref, get } from 'firebase/database';
 
 function BookingPage() {
+    const { tutorId } = useParams();
+    const db = getDatabase();
 
     const [date, setDate] = useState(new Date());// react help use to set date as selected date when we recall on change
     const [selectedTime, setSelectedTime] = useState(null); // the current selected time
+    const [timeSlots, setTimeSlots] = useState([]);
 
-    const timeSlots = [
+    const morningSlots = [
         "09:00 AM",
         "10:00 AM",
         "11:00 AM",
+
+
+
+    ];
+    const afternoonSlots = [
+
         "12:00 PM",
         "01:00 PM",
         "02:00 PM",
         "03:00 PM",
         "04:00 PM",
+        "05:00 PM",
+
     ];
+    const eveningSlots = [
+
+        "06:00 PM",
+        "07:00 PM",
+
+    ];
+    const allDaySlots = morningSlots.concat(afternoonSlots).concat(eveningSlots);
+
 
     const handleTimeClick = (time) => {
         if (selectedTime === time) {
@@ -35,27 +55,69 @@ function BookingPage() {
 
 
     };
+
+    const [data, setData] = useState(null);
+
+    const fetchData = async (recordId) => {
+        try {
+            const recordRef = ref(db, `tutors/${recordId}`);
+            const snapshot = await get(recordRef);
+
+            if (snapshot.exists()) {
+                return snapshot.val();
+            } else {
+                console.log('No data available');
+                return null;
+            }
+        } catch (err) {
+            return null;
+        } finally {
+        }
+    };
+
+
+
+
+    fetchData(tutorId).then(
+        (data) => {
+            setData(data);
+            if (data.availableTime === "morning")
+                setTimeSlots(morningSlots);
+            else if (data.availableTime === "afternoon")
+                setTimeSlots(afternoonSlots);
+            else if (data.availableTime === "evening")
+                setTimeSlots(eveningSlots);
+            else
+                setTimeSlots(allDaySlots);
+        }
+    )
+
     return (
         <div className="container booking-container">
             <div className="row">
                 <div className="col-4 left-part" >
-                    <div className="left-content">
-                        <img id="tutor-img" src={tutor} alt="tutor" />
-                        <div>
-                            <button className="tutor-name">Tutor-A</button>
-                        </div>
-                        <div className="course mt-5">Tutoring-Physics</div>
-
-                        <div className="clock-money">
+                    {data != null ?
+                        (<div className="left-content">
+                            <img id="tutor-img" src={data.photo} alt="tutor" />
                             <div>
-                                <img className="icon" src={clock} alt="clock" />30 minutes
+                                <button className="tutor-name">{data.username}</button>
                             </div>
-                            <div>
-                                <img className="icon" src={money} alt="money" />Free
-                            </div>
-                        </div>
+                            <div className="course mt-5">{data.primarySubject}</div>
 
-                    </div>
+                            <div className="clock-money">
+                                <div>
+                                    <img className="icon" src={clock} alt="clock" />{data.availableTime}
+                                </div>
+                                <div>
+                                    <img className="icon" src={money} alt="money" />{data.price}
+                                </div>
+                            </div>
+
+                        </div>)
+
+                        : (<></>)
+                    }
+
                 </div>
                 <div className="col-8 right-part" >
                     <div className="right-content">
