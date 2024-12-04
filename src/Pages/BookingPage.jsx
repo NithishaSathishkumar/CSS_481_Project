59ef1f0a4627da35fe7389abb886f8d605890dc1
andrewlim0619 <114616307+andrewlim0619@users.noreply.github.com>
2024-12-03 17:19:49 -0800

    Done

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../Styling/BookingPage.css';
 import tutor from '../assets/16.png';
 import clock from '../assets/17.png';
@@ -8,23 +8,40 @@ import chat from '../assets/chat.png';
 import globe from '../assets/globe.png';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { Link } from 'react-router-dom';  // import Link
+import { Link, useParams } from 'react-router-dom';  // import Link
+import { getDatabase, ref, get } from 'firebase/database';
 
 function BookingPage() {
+    const { tutorId } = useParams();
+    const db = getDatabase();
 
     const [date, setDate] = useState(new Date());// react help use to set date as selected date when we recall on change
     const [selectedTime, setSelectedTime] = useState(null); // the current selected time
+    const [timeSlots, setTimeSlots] = useState([]);
 
-    const timeSlots = [
+    const morningSlots = [
         "09:00 AM",
         "10:00 AM",
         "11:00 AM",
+    ];
+
+    const afternoonSlots = [
         "12:00 PM",
         "01:00 PM",
         "02:00 PM",
         "03:00 PM",
         "04:00 PM",
+        "05:00 PM",
+
     ];
+
+    const eveningSlots = [
+        "06:00 PM",
+        "07:00 PM",
+    ];
+    
+    const allDaySlots = morningSlots.concat(afternoonSlots).concat(eveningSlots);
+
 
     const handleTimeClick = (time) => {
         if (selectedTime === time) {
@@ -35,58 +52,110 @@ function BookingPage() {
 
 
     };
+
+    const [data, setData] = useState(null);
+
+    const fetchData = async (recordId) => {
+        try {
+            const recordRef = ref(db, `tutors/${recordId}`);
+            const snapshot = await get(recordRef);
+
+            if (snapshot.exists()) {
+                return snapshot.val();
+            } else {
+                console.log('No data available');
+                return null;
+            }
+        } catch (err) {
+            return null;
+        } finally {
+        }
+    };
+
+
+
+
+    fetchData(tutorId).then(
+        (data) => {
+            setData(data);
+            if (data.availableTime === "morning")
+                setTimeSlots(morningSlots);
+            else if (data.availableTime === "afternoon")
+                setTimeSlots(afternoonSlots);
+            else if (data.availableTime === "evening")
+                setTimeSlots(eveningSlots);
+            else
+                setTimeSlots(allDaySlots);
+        }
+    )
+
     return (
-        <div className="container booking-container">
-            <div className="row">
-                <div className="col-4 left-part" >
-                    <div className="left-content">
-                        <img id="tutor-img" src={tutor} alt="tutor" />
+        <div className="BookingConfirmationRootContainer">
+            <div className="BookingConfirmationMainContent">
+                {data != null ?
+                    (<div className="BookingPageLeftContent">
+                        <img id="BookingTutorPP" src={data.photo} alt="tutor"/>
                         <div>
-                            <button className="tutor-name">Tutor-A</button>
-                        </div>
-                        <div className="course mt-5">Tutoring-Physics</div>
-
-                        <div className="clock-money">
-                            <div>
-                                <img className="icon" src={clock} alt="clock" />30 minutes
-                            </div>
-                            <div>
-                                <img className="icon" src={money} alt="money" />Free
-                            </div>
+                            <button className="BookingTutorsName">{data.username}</button>
                         </div>
 
-                    </div>
-                </div>
-                <div className="col-8 right-part" >
-                    <div className="right-content">
-                        <div className="text-start mb-4"><img className="icon" src={inperson} ></img>
-                            <select className="inperson-select">
+                        <div className="BookingTutorsCourse">Tutoring: {data.primarySubject}</div>
+
+                        <div className="BookingDetailsClockMoney">
+                            <div className="BookingDetailsItem">
+                                <img className="BookingDetailsIcon" src={clock} alt="clock" />{data.availableTime}
+                            </div>
+
+                            <div className="BookingDetailsItem">
+                                <img className="BookingDetailsIcon" src={money} alt="money" />{data.price}
+                            </div>
+                        </div>
+
+                    </div>)
+
+                    : (<></>)
+                }
+
+                <div className="BookingPageRightContent">
+                    <div className="BookingPageMeetingOption">
+                            <img className="BookingPersonIcon" src={inperson}></img>
+                            <select className="OnlineInPersonSelect">
                                 <option>In-Person</option>
                                 <option>Online</option>
                             </select>
+                    </div>
 
-                        </div>
-                        <div className="row">
-                            <div className="col-6">
-                                <h3>Select a date and time</h3>
-                                <div className="calendar-wrapper">
-                                    <Calendar onChange={setDate} value={date} />
+                    <div className="BookingPageRR">
+                        <div className="BookingPageLRContent">
+                            <div className="row">
+                                <div className="col-6">
+                                    <h3>Select a date and time</h3>
 
+                                    <div className="BookingCalendar">
+                                        <Calendar onChange={setDate} value={date} />
+                                    </div>
+
+                                    <div className="BookingTimeZone">
+                                        <h4>Time Zone</h4>
+                                        <div className="BookingTimeCont">
+                                            <select className="BookingTimeZoneSelect">
+                                                <option>America/New_york(EDT)</option>
+                                                <option>Pacific Time (PT)</option>
+                                            </select>
+
+                                            <Link to="/faq">
+                                                <button className="BookingHelpButton">FAQ/Help?</button>
+                                            </Link>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="time-zone">
-                                    <h4>Time Zone</h4>
-                                    <select>
-                                        <option>America/New_york(EDT)</option>
-                                        <option>Pacific Time (PT)</option>
-                                    </select>
-                                </div>
-                                <Link to="/faq">
-                                    <button className="help">FAQ/Help</button>
-                                </Link>
-                            </div>
-                            <div className="col-6">
+                            </div >
+                        </div >
+
+                        <div className="BookingPageRRContent">
+                            <div className="BookingPageButtons">
                                 <h3>{date.toDateString()}</h3>
-                                <div className="time-list">
+                                <div className="BookingPageAvailableTimes">
                                     {timeSlots.map((time) => (
                                         <button
                                             key={time}
@@ -98,22 +167,18 @@ function BookingPage() {
                                         </button>
                                     ))}
                                 </div>
-                                <Link
-                                    to="/confirmation"
-                                    state={{ date: date.toDateString(), time: selectedTime }}
-                                >
-                                    <button className="btn-confirm" disabled={!selectedTime}>
-                                        CONFIRM
-                                    </button>
-                                </Link>
-                                <img className="chat" src={chat}></img>
-                            </div >
-                        </div >
-                    </div >
-                </div >
+                            </div>
 
-            </div >
-        </div >
+                            <div class="BookingConfirmButtonContainer">
+                                <Link to="/confirmation" state={{ date: date.toDateString(), time: selectedTime }}>
+                                        <button className="BookingConfirmButton" disabled={!selectedTime}>Confirm</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
