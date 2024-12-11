f19095c3ab9498a4bc1cdd9f1502bd038f6ea7e5
NithishaSathishkumar <nithisha132004@gmail.com>
2024-12-11 13:28:08 -0800

    added comment

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -12,7 +12,6 @@ import inperson from '../assets/inperson.png';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-
 function BookingPage() {
     // Retrieve the `tutorId` from the route parameters
     const { tutorId } = useParams();
